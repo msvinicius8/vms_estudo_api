f4b34f72a653871a5e62bcbde4797814da2d99bc
msvinicius8 <58345491+msvinicius8@users.noreply.github.com>
2021-04-27 10:40:26 -0300

    Add files via upload

---
+++ forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+public class AtualizacaoTopicoForm {
+	
+	@NotNull @NotEmpty @Length(min = 5)
+	private String titulo;
+	
+	@NotNull @NotEmpty @Length(min = 10)
+	private String mensagem;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getOne(id); // traz o método antigo, versão atual do banco
+		topico.setTitulo(this.titulo); // sobrescreve com os dados vindos do JSon do usuário
+		topico.setMensagem(this.mensagem);
+		return topico; 
+	}
+	
+	
+}
---
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,21 @@
+package br.com.alura.forum.repository;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+import br.com.alura.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
+	
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+	Page<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso, Pageable paginacao);
+
+}
